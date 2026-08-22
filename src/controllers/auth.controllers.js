3d77a0264b5b70ed02b463c9e947c6b23ecacde1
Makhdum <hossainmakhdum2004@gmail.com>
2026-08-22 20:27:27 +0530

    Add email service for registration confirmation via Nodemailer library

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,5 +1,7 @@
 const userModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
+const emailService = require("../services/email.service");
+
 /*
 @desc Register a new user
 @route POST /api/auth/register
@@ -30,6 +32,9 @@ async function userRegisterController(req, res){
         }, 
         token
     })
+    
+    // Send registration email
+    await emailService.sendRegistrationEmail(user.email, user.name);
 }
 /*
 @desc Login a user
