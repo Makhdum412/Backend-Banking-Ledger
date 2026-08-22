fb1f0c140d43ed3aa3db33ac8c2dc4efbaff8a30
Makhdum <hossainmakhdum2004@gmail.com>
2026-08-23 02:05:54 +0530

    feat: add account module (model, controller, routes) and mount to app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
+
+
+//TODO: Add authentication routes
 const authRouter = require("./routes/auth.routes")
+// TODO: Add account routes
+const accountRouter = require("./routes/account.routes")
 
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
+
+// Routes
 app.use("/api/auth", authRouter);
+app.use("/api/account", accountRouter);
 
 module.exports = app;
---
+++ src/models/account.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const accountSchema = new mongoose.Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        index: true
+    },
+    status:{
+        type: String,
+        enum: {
+            values: ["ACTIVE", "FROZEN", "CLOSED"],
+            message: "Status must be active, inactive, or suspended"
+        },
+        default: "ACTIVE"
+    },
+    currency:{
+        type: String,
+        required: [true, "Currency is required for creating an account"],
+        default: "INR"
+    }
+},{
+    timestamps: true
+})
+accountSchema.index({user:1, status:1})
+const accountModel = mongoose.model("account", accountSchema);
+
+module.exports = accountModel;
